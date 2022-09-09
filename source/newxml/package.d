bb5f486b160ce2bfb9b46c7e70d8fecf684ade40
ZILtoid1991 <laszloszeremi@outlook.com>
2022-09-09 23:36:29 +0200

    Added `package.d` for easy importing

---
+++ source/newxml/package.d
@@ -0,0 +1,11 @@
+module newxml;
+
+public import newxml.dom;
+public import domimpl = newxml.domimpl;
+public import newxml.domparser;
+public import newxml.domstring;
+public import newxml.sax;
+public import newxml.writer;
+public import newxml.cursor;
+public import newxml.lexers;
+public import newxml.parser;
